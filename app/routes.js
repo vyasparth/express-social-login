002b14ba56a5d4d7858c904f1216a934bf4f95bb
Parth Vyas <parth.vyas@solutelabs.com>
2017-02-23 19:08:00 +0000

    Bug fix facebook login details

--- app/routes.js
+++ app/routes.js
@@ -25,7 +25,7 @@ module.exports = function(app, passport) {
     // FACEBOOK ROUTES =====================
     // =====================================
     // route for facebook authentication and login
-    app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['email','user_friends'] }));
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['email'] }));
 
     // handle the callback after facebook has authenticated the user
     app.get('/auth/facebook/callback',
--- config/auth.js
+++ config/auth.js
@@ -4,12 +4,11 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'        : '134453110287566', // your App ID
-        'clientSecret'    : '64ce54dd6188650ded6a90a5d7a268fc', // your App Secret
+        'clientID'        : '', // your App ID
+        'clientSecret'    : '', // your App Secret
         'callbackURL'     : 'http://localhost:3000/auth/facebook/callback',
-        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
-        // "passReqToCallback" : true,
-        'profileFields': ['email']
+        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
+
     },
 
     'twitterAuth' : {
@@ -19,8 +18,8 @@ module.exports = {
     },
 
     'googleAuth' : {
-        'clientID'         : '921550211190-5blbjop36hgt6o7fk8d228g1c9irs7an.apps.googleusercontent.com',
-        'clientSecret'     : 'FBL7eo0c_EaXJU-nJE-6dGMo',
+        'clientID'         : '', // your Client ID
+        'clientSecret'     : '', // your Client Secret
         'callbackURL'      : 'http://localhost:3000/auth/google/callback'
     }
 
